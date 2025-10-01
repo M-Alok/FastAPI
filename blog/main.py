025215409641ceaa31d182bb52a3d67c45eba62d
Alok <ams03022001@gmail.com>
2025-10-01 22:01:51 +0530

    Relationships in FastAPI SQL batabase

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
 from .import schemas, models, database
 from sqlalchemy.orm import Session
+from typing import List
+from passlib.context import CryptContext
+from bcrypt import gensalt, hashpw
 
 app = FastAPI()
 
@@ -13,27 +16,29 @@ def get_db():
 
 models.Base.metadata.create_all(database.engine)
 
-@app.post('/create', status_code=status.HTTP_201_CREATED)
+password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+@app.post('/create', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def creat_blog(request: schemas.Blog, db: Session = Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-@app.get('/blogs')
+@app.get('/blogs', status_code=status.HTTP_200_OK, response_model=List[schemas.ShowBlog], tags=['blogs'])
 def all_blogs(db: Session = Depends(get_db)):
     blogs = db.query(models.Blog).all()
     return blogs
 
-@app.get('/blog/{id}')
+@app.get('/blog/{id}', status_code=status.HTTP_200_OK, response_model=schemas.ShowBlog, tags=['blogs'])
 def get_blog(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog of id {id} is not found')
     return blog
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first()
     if not blog:
@@ -46,10 +51,31 @@ def update_blog(id, request: schemas.Blog, db: Session = Depends(get_db)):
     db.refresh(blog)
     return blog
 
-@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT)
+@app.delete('/blog/{id}', status_code=status.HTTP_204_NO_CONTENT, tags=['blogs'])
 def delete_blog(id, db: Session = Depends(get_db)):
     blog = db.query(models.Blog).filter(models.Blog.id == id).first().delete(synchronize_session=False)
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'Blog of id {id} is not found')
     db.commit()
     return {'detail': f'Blog of id {id} is deleted'}
+
+def hash_password(password: str):
+    salt = gensalt()
+    hashed_password = hashpw(password.encode('utf-8'), salt)
+    return hashed_password.decode('utf-8')
+
+@app.post('/user', response_model=schemas.ShowUser, tags=['users'])
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    hashedPassword = hash_password(request.password)
+    new_user = models.User(name=request.name, email=request.email, password=hashedPassword)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+@app.get('/user/{id}', response_model=schemas.ShowUser, tags=['users'])
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'User not found')
+    return user
